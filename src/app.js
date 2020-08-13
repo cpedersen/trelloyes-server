d79f4e0d8f20dd0107ca441e432e3e2d5305da66
Catherine Pedersen <cathy.pedersen@gmail.com>
2020-08-12 23:03:16 -0400

    Misc

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-/*const winston = require('winston');*/
 const { v4: uuid } = require('uuid');
 const cardRouter = require('./card/card-router')
 const listRouter = require('./list/list-router')
@@ -39,37 +38,9 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-//Set card router after validateBearerToken
+//Set router after validateBearerToken
 app.use(cardRouter)
 app.use(listRouter)
-
-/* -------------------------------------------------------- */
-/*                    Test Data                             */
-/* -------------------------------------------------------- */
-/*const cards = [{
-  id: 1,
-  title: 'Task One',
-  content: 'This is card one'
-},
-{
-  id: 2,
-  title: 'Task Two',
-  content: 'This is card two'
-}
-];*/
-
-/*const lists = [{
-  id: 1,
-  header: 'List One',
-  cardIds: [1]
-},
-{
-  id: 2,
-  header: 'List Two',
-  cardIds: [2]
-}
-];
-*/
 
 /* -------------------------------------------------------- */
 /*                         GET /                            */
@@ -77,132 +48,6 @@ app.use(listRouter)
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
-
-/* -------------------------------------------------------- */
-/*                       CARD ID                            */
-/* -------------------------------------------------------- */
-/*app.get('/card/:id', (req, res) => {
-  const { id } = req.params;
-  const card = cards.find(c => c.id == id);
-  if (!card) {
-    logger.error(`Card with id ${id} not found.`);
-    return res
-      .status(404)
-      .send('Card Not Found');
-  }
-  res.json(card);
-});
-
-
-app.delete('/card/:id', (req, res) => {
-  const { id } = req.params;
-  const cardIndex = cards.findIndex(li => li.id == id);
-  if (cardIndex === -1) {
-    logger.error(`Card with id ${id} not found.`);
-    return res
-      .status(404)
-      .send('Not Found');
-  }
-  cards.splice(cardIndex, 1);
-  logger.info(`Card with id ${id} deleted.`);
-  res
-    .status(204)
-    .end();
-});*/
-
-
-/* -------------------------------------------------------- */
-/*                          LIST                            */
-/* -------------------------------------------------------- */
-/*app.get('/list', (req, res) => {
-  res
-    .json(lists);
-});
-
-
-app.get('/list/:id', (req, res) => {
-  const { id } = req.params;
-  const list = lists.find(li => li.id == id);
-
-  // make sure we found a list
-  if (!list) {
-    logger.error(`List with id ${id} not found.`);
-    return res
-      .status(404)
-      .send('List Not Found');
-  }
-
-  res.json(list);
-});
-
-
-app.post('/list', (req, res) => {
-  const { header, cardIds = [] } = req.body;
-
-  if (!header) {
-    logger.error(`Header is required`);
-    return res
-      .status(400)
-      .send('Invalid data');
-  }
-
-  // check card IDs
-  if (cardIds.length > 0) {
-    let valid = true;
-    cardIds.forEach(cid => {
-      const card = cards.find(c => c.id == cid);
-      if (!card) {
-        logger.error(`Card with id ${cid} not found in cards array.`);
-        valid = false;
-      }
-    });
-
-    if (!valid) {
-      return res
-        .status(400)
-        .send('Invalid data');
-    }
-  }
-
-  // get an id
-  const id = uuid();
-
-  const list = {
-    id,
-    header,
-    cardIds
-  };
-
-  lists.push(list);
-
-  logger.info(`List with id ${id} created`);
-
-  res
-    .status(201)
-    .location(`http://localhost:8000/list/${id}`)
-    .json({id});
-});
-
-app.delete('/list/:id', (req, res) => {
-  const { id } = req.params;
-
-  const listIndex = lists.findIndex(li => li.id == id);
-
-  if (listIndex === -1) {
-    logger.error(`List with id ${id} not found.`);
-    return res
-      .status(404)
-      .send('Not Found');
-  }
-
-  lists.splice(listIndex, 1);
-
-  logger.info(`List with id ${id} deleted.`);
-  res
-    .status(204)
-    .end();
-});
-*/
 
 /* -------------------------------------------------------- */
 /*                 Error Handler                            */
